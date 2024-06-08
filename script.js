d6d7ed6b2f8b0841628500ebc752b5f2d7d959c7
Giogamm <giovanymemije@gmail.com>
2024-06-07 22:09:19 -0600

    actu obj

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector("#mobile-menu");
+  const navList = document.querySelector(".nav-list");
+
+  menu.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+});
